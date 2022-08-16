2c3615da482457a705d06e9890ee5e223bf27159
Patrick Koers <pkoers75@gmail.com>
2022-08-16 15:13:45 +0100

    working code with no style

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#tasks'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
